918222ab6f027d1828480ef70670e6420e0df224
enrique <enrique@localhost>
2021-01-22 12:55:45 +0100

    Corrección de errores

--- Tema 2/moduladorAM.m
+++ Tema 2/moduladorAM.m
@@ -1,21 +1,15 @@
-function [y, xc] = moduladorAM(x, Ac, fc, mu, fs)
-%function [y, xc] = moduladorAM(x, Ac, fc, mu, fs)
+function y = moduladorAM(x, x_c, m)
+%function y = moduladorAM(x, Ac, m)
 %
 % Modula una señal en AM.
 % Entradas:
 %   - x: Señal moduladora 
-%   - Ac: Amplitud de la portadora (V)
-%   - fc: Frecuencia de la portadora (Hz)
-%   - mu: Índice de modulación
-%   - fs: Frecuencia de muestreo (Hz)
+%   - xc: Portadora
+%   - m: Índice de modulación
 % Salidas:
 %   - y: Señal modulada en AM
-%   - xc: Portadora
 
-Ts = 1/fs;
-t = 0:Ts:(Ts*length(x))-Ts; % Genero el vector de tiempos;
-xn = x/max(abs(x));         % Normalizo la señal de entrada
+x_n = x/max(abs(x));         % Normalizo la señal de entrada (obligatorio en AM)
 
-xc = Ac*cos(2*pi*fc.*t);    % Portadora
-y = (1 + mu*xn).*xc;        % Señal modulada
+y = (1 + m*x_n).*x_c;        % Señal modulada
 
--- Tema 2/moduladorDBL.m
+++ Tema 2/moduladorDBL.m
@@ -1,18 +1,11 @@
-function [y, xc] = moduladorDBL(x, Ac, fc, fs)
-%function [y, xc] = moduladorAM(x, Ac, fc, fs)
+function y = moduladorDBL(x, x_c)
+%function y = moduladorAM(x, x_c)
 %
 % Modula una señal en DBL.
 % Entradas:
 %   - x: Señal moduladora 
-%   - Ac: Amplitud de la portadora (V)
-%   - fc: Frecuencia de la portadora (Hz)
-%   - fs: Frecuencia de muestreo (Hz)
+%   - x_c: Portadora
 % Salidas:
 %   - y: Señal modulada en DBL
-%   - xc: Portadora
 
-Ts = 1/fs;
-t = 0:Ts:(Ts*length(x))-Ts; % Genero el vector de tiempos;
-
-xc = Ac*cos(2*pi*fc.*t);    % Portadora
-y = x.*xc;                  % Señal modulada
+y = x.*x_c;                  
